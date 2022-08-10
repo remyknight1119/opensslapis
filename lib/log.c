5b6eee868fab9ee9d0086276ed4178ea70479909
remy <remyknight1119@gmail.com>
2022-08-10 14:28:59 -0700

    pkcs12 certs
    Signed-off-by: remy <remyknight1119@gmail.com>

--- lib/log.c
+++ lib/log.c
@@ -12,14 +12,17 @@ static char osslapis_log_buf[OSSAPIS_LOG_BUF_LEN];
 
 const char *osslapis_ossl_log_error(void)
 {
+    const char *file = NULL;
+    const char *func = NULL;
     const char *data = NULL;
     const char *reason = NULL;
+    int line = 0;
 
     reason = ERR_reason_error_string(ERR_get_error());
-    ERR_get_error_all(NULL, NULL, NULL, &data, NULL);
+    ERR_get_error_all(&file, &line, &func, &data, NULL);
     if (data != NULL) {
-        snprintf(osslapis_log_buf, sizeof(osslapis_log_buf),
-                "%s: %s", reason, data);
+        snprintf(osslapis_log_buf, sizeof(osslapis_log_buf), "%s(%s:%d %s): %s",
+                reason, file, line, func, data);
     }
 
     return osslapis_log_buf;
